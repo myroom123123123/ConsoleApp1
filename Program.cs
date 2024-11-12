2fc1d98fb0d6a5308f007e45bb010a11c357ea7a
Petrenko Valeriya <mroom9195@gmail.com>
2024-11-12 20:44:29 +0200

    ex3

--- Program.cs
+++ Program.cs
@@ -1,37 +1,22 @@
-﻿namespace Task_2
-
+﻿using System;
+namespace Task_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter 5 numbers: ");
-            int[] numbers = new int[5];
-            for (int i = 0; i < 5; i++)
+            Console.WriteLine("Enter a six-digit number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int reversedNumber = 0;
+            int temp = number;
+
+            for (int i = 0; i < 6; i++)
             {
-                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                reversedNumber = reversedNumber * 10 + temp % 10;
+                temp /= 10;
             }
-            int sum = 0;
-            int max = numbers[0];
-            int min = numbers[0];
-            int mult = 1;
-            for (int i = 0; i < 5; i++)
-            {
-                sum += numbers[i];
-                if (numbers[i] > max)
-                {
-                    max = numbers[i];
-                }
-                if (numbers[i] < min)
-                {
-                    min = numbers[i];
-                }
-                mult *= numbers[i];
-            }
-            Console.WriteLine("Sum: " + sum);
-            Console.WriteLine("Max: " + max);
-            Console.WriteLine("Min: " + min);
-            Console.WriteLine("Mult: " + mult);
+
+            Console.WriteLine($"Reversed number: {reversedNumber}");
         }
     }
 }
