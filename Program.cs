63176d3e4008e77652d120dbe3746066e41d8021
Petrenko Valeriya <mroom9195@gmail.com>
2024-11-12 20:48:23 +0200

    ex4

--- Program.cs
+++ Program.cs
@@ -1,22 +1,27 @@
 ﻿using System;
-namespace Task_3
+namespace Task_4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a six-digit number: ");
-            int number = Convert.ToInt32(Console.ReadLine());
-            int reversedNumber = 0;
-            int temp = number;
-
-            for (int i = 0; i < 6; i++)
+            Console.WriteLine("Enter the range of numbers:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = 0;
+            int b = 1;
+            int c = 0;
+            Console.Write(a + " ");
+            Console.Write(b + " ");
+            for (int i = 0; i < n; i++)
             {
-                reversedNumber = reversedNumber * 10 + temp % 10;
-                temp /= 10;
+                c = a + b;
+                if (c <= n)
+                {
+                    Console.Write(c + " ");
+                    a = b;
+                    b = c;
+                }
             }
-
-            Console.WriteLine($"Reversed number: {reversedNumber}");
         }
     }
 }
