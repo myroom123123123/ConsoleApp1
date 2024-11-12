941a4f51b33a39ee65db2396ee04961714e82843
Petrenko Valeriya <mroom9195@gmail.com>
2024-11-12 20:37:15 +0200

    ex1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿namespace ConsoleApp1
+﻿namespace Task_1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("It's easy to win forgiveness for being wrong;\nbeing right is what gets you into real trouble.\nBjarne Stroustrup");
         }
     }
 }
