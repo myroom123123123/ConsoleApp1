3d55c56901ee50f790c7acb85b90c4572be6760f
Petrenko Valeriya <mroom9195@gmail.com>
2024-11-12 20:55:24 +0200

    ex6

--- Program.cs
+++ Program.cs
@@ -5,24 +5,29 @@ namespace Task_5
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the first number: ");
-            int A = int.Parse(Console.ReadLine());
-            Console.Write("Enter the second number: ");
-            int B = int.Parse(Console.ReadLine());
-            if (A < B)
+            Console.WriteLine("Enter the length of the line: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the symbol of the line: ");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter the direction of the line (horizontal or vertical): ");
+            string direction = Console.ReadLine();
+            if (direction == "horizontal")
             {
-                for (int i = A; i <= B; i++)
+                for (int i = 0; i < length; i++)
                 {
-                    for (int j = 0; j < i; j++)
-                    {
-                        Console.Write(i + " ");
-                    }
-                    Console.WriteLine();
+                    Console.Write(symbol);
+                }
+            }
+            else if (direction == "vertical")
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    Console.WriteLine(symbol);
                 }
             }
             else
             {
-                Console.WriteLine("The first number must be less than the second number");
+                Console.WriteLine("Error");
             }
         }
     }
